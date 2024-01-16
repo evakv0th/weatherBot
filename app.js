184a6c99f1d1ee37a2ee4d628bcdf4c5dcfb9f4f
kvothe eva <evakv0th@gmail.com>
2024-01-16 21:07:13 +0200

    v1.0.1: fix

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
-const moment = require("moment-timezone");
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const OPENWEATHERMAP_API_KEY = process.env.OPENWEATHERMAP_API_KEY;
@@ -14,12 +13,11 @@ bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(
     chatId,
-    "Hello! This bot can show you the weather and time for any city. To use it, please choose an option below:",
+    "Hello! This bot can show you the weather for any city.",
     {
       reply_markup: {
         inline_keyboard: [
           [{ text: "Get Weather", callback_data: "get_weather" }],
-          [{ text: "Get Time", callback_data: "get_time" }],
         ],
       },
     }
@@ -40,15 +38,6 @@ bot.on("callback_query", async (callbackQuery) => {
         "Please enter the name of the city or send /stop to cancel:"
       );
       break;
-    case "get_time":
-      const userDataTime = getUserData(chatId);
-      userDataTime.waitingForCity = true;
-      userDataTime.waitingForTime = true;
-      bot.sendMessage(
-        chatId,
-        "Please enter the name of the city or send /stop to cancel:"
-      );
-      break;
     default:
       break;
   }
@@ -60,7 +49,6 @@ function getUserData(chatId) {
     userData = {
       waitingForCity: false,
       waitingForWeather: false,
-      waitingForTime: false,
     };
     storage[chatId] = userData;
   }
@@ -75,11 +63,7 @@ bot.on("message", async (msg) => {
   if (userData && userData.waitingForCity) {
     const city = text;
     let messageText = "";
-    if (userData.waitingForWeather) {
-      messageText = await getWeatherData(city);
-    } else if (userData.waitingForTime) {
-      messageText = await getTimeData(city);
-    }
+    messageText = await getWeatherData(city);
     bot.sendMessage(chatId, messageText);
     resetUserData(chatId);
   }
@@ -89,7 +73,6 @@ function resetUserData(chatId) {
   const userData = getUserData(chatId);
   userData.waitingForCity = false;
   userData.waitingForWeather = false;
-  userData.waitingForTime = false;
 }
 
 async function getWeatherData(city) {
@@ -102,13 +85,3 @@ async function getWeatherData(city) {
   const messageText = `The weather in ${city} is currently ${weatherDescription} with a temperature of ${temperature}°C.`;
   return messageText;
 }
-
-async function getTimeData(city) {
-    const response = await axios.get(
-      `http://api.geonames.org/timezoneJSON?formatted=true&lat=${city.lat}&lng=${city.lon}&username=demo&style=full`
-    )
-    const data = response.data
-    const localTime = data.time
-    const messageText = `The current time in ${city} is ${localTime}.`
-    return messageText
-  }
